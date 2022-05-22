dd70d01229738390bccc8baae4537ec52b3aae3b
maggie-msc <99621521+maggie-msc@users.noreply.github.com>
2022-05-22 22:11:40 +0100

    add show and hide textarea function

---
+++ script.js
@@ -0,0 +1,8 @@
+function showInputBox() {
+    var x = document.getElementById("input");
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+    } else {
+        x.style.display = "none";
+    }
+}
