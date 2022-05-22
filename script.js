d8c2ab9e29ac91d9b12f479e47726bd17f40490c
maggie-msc <99621521+maggie-msc@users.noreply.github.com>
2022-05-22 23:33:55 +0100

    Modify showInputBox function
    new element created when button clicked
    
    - Find way change onclick function

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+count = 0
 function showInputBox() {
-    var x = document.getElementById("input");
-    if (x.style.display === "none") {
-        x.style.display = "inline-block";
-    } else {
-        x.style.display = "none";
-    }
+    var newInput = document.createElement("textarea");
+    newInput.className = "input";
+    newInput.id = "input"+count;
+    newInput.name = "input";
+    newInput.cols = 30;
+    newInput.rows = 10;
+    document.body.appendChild(newInput);
+    // newInput.style.display = "inline-block";
+    console.log(newInput.className, newInput.id);
+    // var x = document.getElementById(newInput.id);
+    // if (newInput.style.display === "none") {
+    //     newInput.style.display = "inline-block";
+    // } else {
+    //     newInput.style.display = "none";
+    // }
+
+    var button = document.getElementById("button");
+    // button.onclick = "completeInput("+newInput.id+")";
+    button.onclick = "completeInput()";
+}
+
+// function completeInput(inputId) {
+function completeInput() {
+    var input = document.getElementById("input0");
+    input.style.marginLeft = "20px";
+    input.style.marginTop = "10px";
+    count+=1
+
 }
