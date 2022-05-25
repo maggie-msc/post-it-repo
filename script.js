181a0ee1d2346601cb3ff2e77c1fbb0849e93449
maggie-msc <99621521+maggie-msc@users.noreply.github.com>
2022-05-25 16:56:31 +0100

    move box's position to grid layout

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 count = 0
 function showInputBox() {
     var newInput = document.createElement("textarea");
+    var footer = document.getElementById("footer");
     newInput.className = "input";
-    newInput.id = "input"+count;
+    newInput.id = "input-"+count;
     newInput.name = "input";
     newInput.cols = 30;
     newInput.rows = 10;
-    document.body.appendChild(newInput);
-    // newInput.style.display = "inline-block";
-    console.log(newInput.className, newInput.id);
-    // var x = document.getElementById(newInput.id);
-    // if (newInput.style.display === "none") {
-    //     newInput.style.display = "inline-block";
-    // } else {
-    //     newInput.style.display = "none";
-    // }
-
+    newInput.style.position = "absolute";
+    footer.appendChild(newInput);
     var button = document.getElementById("button");
-    // button.onclick = "completeInput("+newInput.id+")";
-    button.onclick = "completeInput()";
+    button.onclick = function(){completeInput(newInput.id)};
 }
 
-// function completeInput(inputId) {
-function completeInput() {
-    var input = document.getElementById("input0");
+function completeInput(input_id) {
+    var input = document.getElementById(input_id);
+    input.style.position = "relative";
     input.style.marginLeft = "20px";
     input.style.marginTop = "10px";
     count+=1
+    var button = document.getElementById("button");
+    button.onclick = function(){showInputBox()};
+
 
 }
+
+// change class name to something else after 2nd function and change postion during
+// first function - first time should be inline = only in the grid
