3720892a47e0dbb438d4100d95aac1e2bfbecacd
maggie-msc <99621521+maggie-msc@users.noreply.github.com>
2022-06-11 00:34:19 +0100

    textarea remains in place
    (when post-it added to body)

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ function showInputBox() {
     var newInput = document.createElement("textarea");
     var footer = document.getElementById("footer");
     newInput.className = "input";
-    newInput.id = "input-"+count;
+    newInput.id = "input";
     newInput.name = "input";
     newInput.cols = 30;
     newInput.rows = 10;
     newInput.style.position = "absolute";
+    newInput.style.display = "inline-block";
     footer.appendChild(newInput);
     var button = document.getElementById("button");
-    button.onclick = function(){completeInput(newInput.id)};
+    button.onclick = function(){completeInput()};
 }
 
-function completeInput(input_id) {
-    var input = document.getElementById(input_id);
-    input.style.position = "relative";
-    input.style.marginLeft = "20px";
-    input.style.marginTop = "10px";
+function completeInput() {
+    var input = document.getElementById("input");
+    var displayInput = document.createElement("textarea");
+    displayInput.className = "input-to-display";
+    displayInput.id = "post";
+    displayInput.name = "post";
+    displayInput.cols = 30;
+    displayInput.rows = 10;
+
+    // displayInput.style.position = "relative";
+    // displayInput.style.marginLeft = "20px";
+    // displayInput.style.marginTop = "10px";
     count+=1
+    input.remove()
+    document.body.appendChild(displayInput);
     var button = document.getElementById("button");
     button.onclick = function(){showInputBox()};
 
